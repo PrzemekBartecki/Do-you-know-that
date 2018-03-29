3d475b834a9f15f6c0f1113934329aae7038028c
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2018-03-29 18:34:18 +0200

    new look

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	var curio = document.querySelector("#curio");
 	var curioArray = [
-		"ciekawostka 1",
-		"ciekawostka 2",
-		"ciekawostka 3",
-		"ciekawostka 4",
-		"Lorem ipsum dolor sit amet nunc venenatis tristique, urna viverra sit amet nibh. Maecenas in metus. Aliquam orci. Vestibulum non eros. Mauris pretium wisi, eu quam. Nulla dolor sit amet mauris eget felis non nibh wisi, dapibus risus. Suspendisse in sollicitudin lorem, at libero. In ornare euismod. Integer non felis. Donec libero a dolor. Mauris eget leo ac dolor. Nam id sapien eu tincidunt nonummy, tellus nec felis laoreet commodo, tortor vehicula viverra. Cras aliquet. Quisque vitae ligula tempor vitae, congue risus vel sodales arcu, eget volutpat quis, interdum wisi at fermentum ut, blandit velit wisi, aliquam iaculis pede cursus tristique, urna dapibus tellus. Morbi accumsan rutrum, libero odio eget sapien pede bibendum tellus, at quam. Phasellus tellus dui at tellus wisi, mollis lobortis. In cursus ante. Proin cursus non, posuere dui. Suspendisse et magnis dis parturient montes, nascetur ridiculus mus. Nam molestie. Vivamus orci sit amet wisi. Mauris ultrices. Sed diam iaculis et, enim. Duis at neque. Nunc placerat porta tincidunt",
-		"ciekaowstka 5",
+		"Międzynarodowa Federacja Kynologiczna uznaje obecnie 332 rasy psów.",
+		"W 1015 roku w kierunku Wysp Brytyjskich ruszyła flota wikingów tak wielka, że podobnej nigdy jeszcze nie widziano. Źródła różnią się w ocenie, ile statków liczyła – padają liczby od 200 do 1000. Na jej czele stał Knut, około osiemnastoletni brat króla Danii. Rzeczywiście, płynęła w nim piastowska krew, bo jego matka – niesłusznie zwana Świętosławą – była córką Mieszka I i siostrą Bolesława Chrobrego. Oczywiście, „pół-Polak” to chwyt retoryczny, bo na podobnej zasadzie Bolesławowie Chrobry i Krzywousty byli „pół-Czechami”, Kazimierzowie Odnowiciel i Sprawiedliwy „pół-Niemcami”, a królowa Jadwiga Andegaweńska „pół-Bośniaczką”.",
+		"Enigmę została złamana przez trójkę polaków: Mariana Rejewskiego, Jerzego Różyckiego i Henryka Zygalskiego ",
+		"Najdłuższy wyraz na świecie składa się z 189,819 liter. Wyraz ten jest chemiczną nazwą tytyny, czyli długiego, włóknistego białka mięśni poprzecznie prążkowanych u kręgowców. Tytyna jest największym znanym białkiem. Wymówienie pełnej nazwy zajmuje 3 godziny i 33 minuty.",
+		"To czy chleb spadnie na podłogę posmarowaną stroną zależy od wysokości stołu ",
 			""
 		];
 	var arrayLenght= (curioArray.length -1);
